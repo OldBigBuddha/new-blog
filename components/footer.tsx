ec2896b6323f5471dc22311b43287858957f85be
OldBigBuddha <development@oldbigbuddha.net>
2020-12-04 13:03:17 +0900

    [update] footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,28 +3,27 @@ import { EXAMPLE_PATH } from '../lib/constants'
 
 const Footer = () => {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
-        </div>
-      </Container>
+    <footer className="flex bg-accent-1 border-t border-accent-2 justify-between">
+      <div className="flex flex-col lg:flex-row">
+        <span className="mx-3">License: <a className="font-bold hover:underline" href="https://creativecommons.org/licenses/by-sa/4.0/">CC-BY-SA 4.0</a></span>
+      </div>
+      <div>
+        <small>&copy; 2020 OldBigBuddha.</small>
+      </div>
+      <div className="flex flex-col lg:flex-row">
+        <a
+          href="#"
+          className="mx-3 font-bold hover:underline"
+        >
+          Privacy Policy
+        </a>
+        <a
+          href="https://github.com/OldBigBuddha/Blog"
+          className="mx-3 font-bold hover:underline"
+        >
+          View on GitHub
+        </a>
+      </div>
     </footer>
   )
 }
