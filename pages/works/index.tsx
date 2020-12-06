6969c3d075ec17aa161a18ef538ee4d2df13c89e
OldBigBuddha <development@oldbigbuddha.net>
2020-12-06 15:33:36 +0900

    [add] works index

---
+++ pages/works/index.tsx
@@ -0,0 +1,17 @@
+
+import Container from '../../components/container'
+import Layout from '../../components/layout'
+import Head from 'next/head'
+
+const Index: React.FC = () => (
+  <Layout>
+    <Head>
+      <title>OldBigBuddha's Works | Simple is Best</title>
+    </Head>
+    <Container>
+      <h1 className="text-lx text-center">Works</h1>
+    </Container>
+  </Layout>
+)
+
+export default Index;
