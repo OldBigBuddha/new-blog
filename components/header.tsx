60e2a3377110384b04c9e8e834f27be3a8a097b7
OldBigBuddha <development@oldbigbuddha.net>
2020-12-09 14:10:01 +0900

    [update] header

--- components/header.tsx
+++ components/header.tsx
@@ -1,21 +1,29 @@
-import Link from 'next/link'
+import { Box, Flex, HStack, Link, Image, useColorMode } from "@chakra-ui/react";
 
-import DarkModeSwitcher from "./dark-mode-switcher"
-
-import style from '../styles/header.module.css'
+import DarkModeSwitcher from "./dark-mode-switcher";
 
 const Header = () => {
+  const {colorMode} = useColorMode();
   return (
-    <header className="flex justify-between mx-6 mb-10 mt-4">
-      <Link href="/">
-        <a className="flex text-gray-900 font-bold text-2xl md:text-4xl tracking-tight md:tracking-tighter leading-tight rounded-full p-4 transition-shadow hover:shadow hover:no-underline">
-          <img className={style.logo} src="/img/logo.png"/>
-          <span className="mt-auto">imple is Best</span>
-        </a>
+    <Flex as="header" justify="space-between" marginX={3} marginBottom={10} marginTop={4}>
+      <Link
+        href="/"
+        fontSize={{base: "2xl", md: "4xl"}} fontWeight="bold"
+        rounded="full"
+        padding={4}
+        _hover={{
+          textDecoration: "none",
+          boxShadow: "md"
+        }}
+        transitionProperty="box-shadow" transitionDuration="150mx">
+        <HStack spacing={0} align="baseline">
+          <Image h="55px" backgroundColor="transparent" shadow="none" padding={0} src="/img/logo.png"/>
+          <Box as="span" textColor={colorMode == "dark" ? "gray.800" : "gray.200"} marginLeft="0px" marginTop="auto">imple is Best</Box>
+        </HStack>
       </Link>
 
       <DarkModeSwitcher />
-    </header>
+    </Flex>
   )
 }
 
