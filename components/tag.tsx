a971d731de7c4a6ce15e3102c8e407252f655ea9
OldBigBuddha <development@oldbigbuddha.net>
2020-12-13 10:53:01 +0900

    [update] remove tag.module.css

--- components/tag.tsx
+++ components/tag.tsx
@@ -1,14 +1,26 @@
-import { Box } from '@chakra-ui/react';
-import style from '../styles/tag.module.css'
+import { Box, useColorMode } from '@chakra-ui/react';
 
 type Props = {
   tag: string
 }
 
-const Tag: React.FC<Props> = ({tag}: Props) => (
-  <Box className={style.tag}>
-    {tag}
-  </Box>
-)
+const Tag: React.FC<Props> = ({tag}: Props) => {
+
+  const { colorMode } = useColorMode();
+
+  return <Box
+          fontSize={{base: "0.5rem", md: "0.75rem"}}
+          lineHeight="1rem"
+          textColor={colorMode == "dark" ? "gray.600" : "gray.200"}
+          border="1px"
+          borderColor={colorMode == "dark" ? "gray.600" : "gray.200"}
+          rounded="full"
+          textDecoration="no-underline"
+          marginRight={{base: 1, md: 2}}
+          padding={{base: 1, md: 2}}
+          _before={{content: "'#'"}}>
+            {tag}
+          </Box>
+}
 
 export default Tag;
