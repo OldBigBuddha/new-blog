cab7fcbe5fc6aaf48c1b39f92209fde6a9f827a8
OldBigBuddha <development@oldbigbuddha.net>
2021-01-03 09:48:32 +0900

    [add] rss.tsx

---
+++ pages/posts/rss.tsx
@@ -0,0 +1,13 @@
+import { GetServerSideProps } from "next";
+
+const Rss: React.VFC = () => <></>;
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = context.res;
+
+  res.setHeader("Content-Type", "text/xml");
+  res.write("RSS Feed");
+  res.end();
+}
+
+export default Rss;
