afbe6f8d1c2b1bea7cd51716040db4b5e37a97d4
OldBigBuddha <development@oldbigbuddha.net>
2021-01-03 22:18:36 +0900

    [fix] 警告の背景色をダークモードに対応させた

--- components/old-alert.tsx
+++ components/old-alert.tsx
@@ -1,19 +1,22 @@
-import { Box, Text} from "@chakra-ui/react"
+import { Box, Text, useColorMode} from "@chakra-ui/react"
 
 type Props = {
   pastYear: number
 }
 
-const OldAlert: React.FC<Props> = ({ pastYear }: Props) => (
-  <Box
-    backgroundColor="red.200"
-    border={4}
-    borderStyle="solid"
-    borderColor="red.500"
-    rounded="xl"
-    paddingX={4} >
-    <Text>この記事は投稿から {pastYear} 年以上経過しています。<br />最新の情報は各種公式ドキュメント等をご覧下さい。</Text>
-  </Box>
-)
+const OldAlert: React.VFC<Props> = ({ pastYear }: Props) => {
+  const { colorMode } = useColorMode();
+  return (
+    <Box
+      backgroundColor={colorMode == "light" ? "red.800" : "red.200"}
+      border={4}
+      borderStyle="solid"
+      borderColor="red.500"
+      rounded="xl"
+      paddingX={4} >
+      <Text>この記事は投稿から {pastYear} 年以上経過しています。<br />最新の情報は各種公式ドキュメント等をご覧下さい。</Text>
+    </Box>
+  )
+}
 
 export default OldAlert;
