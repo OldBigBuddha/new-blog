abe2abc7462bbc82d0cc0210c7b8cad5fd822731
Shinra Yamakawa <development@oldbigbuddha.net>
2021-08-22 16:06:48 +0900

    [add] smart-iteration-of-js.md (#77)
    * [add] smart-iteration-of-js.md
    
    * ビルドが通るようにした

--- lib/gtag.ts
+++ lib/gtag.ts
@@ -6,23 +6,23 @@ export const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
 export const existsGaId = GA_ID !== "";
 
 // PVを測定する
-export const pageview = (page_path: string) => {
-  if (GA_ID == undefined) {
-    return;
-  }
-  const pagePath: PagePath = { page_path };
-  window.gtag("config", GA_ID, pagePath);
-};
+// export const pageview = (page_path: string) => {
+//   if (GA_ID == undefined) {
+//     return;
+//   }
+//   const pagePath: PagePath = { page_path };
+//   window.gtag("config", GA_ID, pagePath);
+// };
 
 // GAイベントを発火させる
-export const event = ({ action, category, label, value = "" }: ClickEvent) => {
-  if (!existsGaId) {
-    return;
-  }
+// export const event = ({ action, category, label, value = "" }: ClickEvent) => {
+//   if (!existsGaId) {
+//     return;
+//   }
 
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: JSON.stringify(label),
-    value,
-  });
-};
+//   window.gtag("event", action, {
+//     event_category: category,
+//     event_label: JSON.stringify(label),
+//     value,
+//   });
+// };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from 'next/app'
-import * as gtag from '../lib/gtag'
+// import * as gtag from '../lib/gtag'
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import 'highlight.js/styles/atom-one-dark.css'
@@ -8,18 +8,18 @@ import theme from '../theme/index'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
-  useEffect(() => {
-    if (!gtag.existsGaId) return;
+  // useEffect(() => {
+    // if (!gtag.existsGaId) return;
 
-    const handleRouteChange = (path: string) => {
-      gtag.pageview(path);
-    }
+    // const handleRouteChange = (path: string) => {
+    //   gtag.pageview(path);
+    // }
 
-    router.events.on("routeChangeComplete", handleRouteChange)
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange)
-    }
-  }, [router.events])
+  //   router.events.on("routeChangeComplete", handleRouteChange)
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange)
+  //   }
+  // }, [router.events])
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
